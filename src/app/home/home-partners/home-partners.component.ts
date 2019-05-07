6ebaa5a90e522746bc4ce05b6d89c1d401b1b1f3
MichaelWebHub <michael.kutateladze@yandex.ru>
2019-05-07 13:10:26 +0300

    services

--- src/app/home/home-partners/home-partners.component.ts
+++ src/app/home/home-partners/home-partners.component.ts
@@ -9,25 +9,25 @@ export class HomePartnersComponent implements OnInit {
 
   partners: { image: string; url: string; alt: string; title: string }[] = [
     {
-      image: '../../assets/images/Gazprom.png',
+      image: '../../assets/images/partners/Gazprom.png',
       alt: 'Газпром',
       url: 'http://www.gazprom.ru/',
       title: 'Газпром'
     },
     {
-      image: '../../assets/images/Novatek.png',
+      image: '../../assets/images/partners/Novatek.png',
       alt: 'Новатек',
       url: 'http://www.novatek.ru/',
       title: 'Новатек'
     },
     {
-      image: '../../assets/images/Rosneft.png',
+      image: '../../assets/images/partners/Rosneft.png',
       alt: 'Роснефть',
       url: 'https://www.rosneft.ru/',
       title: 'Роснефть'
     },
     {
-      image: '../../assets/images/Sibur.png',
+      image: '../../assets/images/partners/Sibur.png',
       alt: 'Сибур',
       url: 'https://www.sibur.ru/',
       title: 'Сибур'
