71065b7d4368315cfadda8e92baabb5bc0cf8c01
MichaelWebHub <michael.kutateladze@yandex.ru>
2019-05-07 13:35:31 +0300

    preloader

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -10,6 +10,7 @@ import * as emailjs from 'emailjs-com';
 export class ContactsComponent implements OnInit {
 
   status: boolean;
+  showPreloader = false;
 
   constructor(private _http: HttpClient) {
   }
@@ -17,17 +18,24 @@ export class ContactsComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(): void {
+  onSubmit(form): void {
+    this.showPreloader = true;
 
     try {
       emailjs.sendForm('yandex', 'template_NQWXoYQc', 'form', 'user_JOo9rzNDhmu3PM4hhlGed')
         .then((response) => {
           this.status = true;
+          this.showPreloader = false;
+          form.reset();
         }, (err) => {
           this.status = false;
+          this.showPreloader = false;
+          form.reset();
         });
     } catch (e) {
       console.log(e);
+      this.showPreloader = false;
+      form.reset();
     }
 
   }
